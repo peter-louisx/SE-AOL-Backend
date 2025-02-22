ae32afb918eb4f18a329043c900933cf90710ed9
Peter Louis Anderson <peterlouisx@gmail.com>
2025-02-22 23:58:01 +0700

    #feat sanctum

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/token-create-test', function () {
+    $token = User::where('email', 'test@example.com')->first()->createToken('test-token')->plainTextToken;
+    return response()->json(['token' => $token]);
+});
+
+Route::get('/test-auth', function (Request $request) {
+    return response()->json(['message' => 'Hello World!']);
+})->middleware('auth:sanctum');
